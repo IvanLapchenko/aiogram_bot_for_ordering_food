d92d13ab0e3004b1292b9bae8cebe25c0acf62c8
Ivan Lapchenko <lapchenko.vanya@gmail.com>
2023-06-27 19:14:21 +0300

    add ability to register users in db

--- database.py
+++ database.py
@@ -4,35 +4,15 @@ connection = sqlite3.connect('app.db')
 cursor = connection.cursor()
 
 
-def create_database():
-    cursor.execute('CREATE TABLE users (id INTEGER, name VARCHAR(120), phone CHARACTER(20)')
+def create_table():
+    cursor.execute('CREATE TABLE users (id INTEGER, name VARCHAR(120), phone CHARACTER(20))')
 
 
 def create_user_with_name_and_tg_id(name, tg_id):
-    cursor.execute(f'INSERT INTO users (name, id) VALUES({name}, {tg_id})')
+    cursor.execute(f'INSERT INTO users (name, id) VALUES("{name}", "{tg_id}")')
     connection.commit()
 
 
 def add_user_phone(phone, tg_id):
     cursor.execute(f'UPDATE users SET phone={phone} WHERE id={tg_id}')
-    connection.commit()
-
-
-def add_user_id(message):
-    cursor.execute('UPDATE users SET user_id=? WHERE id=?', (message.text, message.chat.id,))
-    connection.commit()
-
-
-def add_user(message):
-    cursor.execute('SELECT id FROM users WHERE id=?', (message.chat.id,))
-    user = cursor.fetchone()
-    if not user:
-        cursor.execute('INSERT INTO users VALUES(?, ?, ?, ?)', (message.chat.id, 'name', 'user_id', 'score'))
-        connection.commit()
-    else:
-        pass
-
-
-def add_username(message):
-    cursor.execute('UPDATE users SET name=? WHERE id=?', (message.text, message.chat.id,))
     connection.commit()
--- handlers.py
+++ handlers.py
@@ -1,9 +1,9 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
-
 from states import RegistrationStates
 from auth_kb import share_phone_keyboard
 from app import dp
+from database import create_user_with_name_and_tg_id, add_user_phone
 
 
 @dp.message_handler(commands=['start'])
@@ -15,15 +15,14 @@ async def start_command(message: types.Message):
 
 @dp.message_handler(state=RegistrationStates.Start)
 async def get_username_and_id(message: types.Message):
-    print(message.text, ' - is user\'s text')
-    print(message.chat.id, ' - is user\'s chat id')
+    create_user_with_name_and_tg_id(message.text, message.from_user.id)
     await RegistrationStates.SharedName.set()
     await message.answer(text='Share your phone number', reply_markup=share_phone_keyboard)
 
 
 @dp.message_handler(state=RegistrationStates.SharedName, content_types=types.ContentType.CONTACT)
 async def get_user_phone_number(message: types.Message):
-    print(message.contact.phone_number)
+    add_user_phone(message.contact.phone_number, message.from_user.id)
     await RegistrationStates.Authorized.set()
     await message.answer(text='Congrats, you are registered user')
 
