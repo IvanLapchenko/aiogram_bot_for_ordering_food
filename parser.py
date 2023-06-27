f2808645ba121297614f5faeec9fc84a70aafa6f
Ivan Lapchenko <lapchenko.vanya@gmail.com>
2023-06-27 20:12:32 +0300

    add function that gets list of information about goods on passed page

---
+++ parser.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_list_of_sushi_info(url):
+    response = requests.get(url)
+    html = BeautifulSoup(response.text, features='html5lib')
+    sushi_divs = html.find_all("div", {"class": "cat-slide swiper-slide"})
+
+    sushi_data = []
+
+    for div in sushi_divs:
+        name = div.select_one('.cat-slide-content > .cat-slide-name > a')
+        weight = div.select_one('.cat-slide-content > .cat-slide-desc > span')
+        price = div.select_one('.cat-slide-content > .cat-slide-buy > .price')
+        image = div.select_one('.cat-image > a > img')
+
+        single_sushi_tuple = (name.text, weight.text, price.text, image.get('src'))
+
+        sushi_data.append(single_sushi_tuple)
+
+    return sushi_data
